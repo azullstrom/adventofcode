9f67cb11e1c57904d07146021f90bfac7e41ca9c
azullstrom <zefaniasmusic@gmail.com>
2022-12-04 20:15:38 +0100

    Day 4 added

---
+++ Day 4/main.py
@@ -0,0 +1,28 @@
+import re
+
+
+if __name__ == '__main__':
+    file = open('input.txt', 'r')
+    input_file = file.readlines()
+    pairs = 0
+    pairs_two = 0
+
+# Part 1
+    for i in range(len(input_file)):
+        input_file[i] = input_file[i].replace('\n', '')
+        sections = re.split(r',|-', input_file[i])
+
+        if int(sections[0]) <= int(sections[2]) <= int(sections[1]) >= int(sections[3]) >= int(sections[0]):
+            pairs += 1
+        elif int(sections[2]) <= int(sections[0]) <= int(sections[3]) >= int(sections[1]) >= int(sections[2]):
+            pairs += 1
+# Part 2
+        if int(sections[0]) <= int(sections[2]) <= int(sections[1]) or int(sections[1]) >= int(sections[3]) >= int(sections[0]):
+            pairs_two += 1
+        elif int(sections[2]) <= int(sections[0]) <= int(sections[3]) or int(sections[3]) >= int(sections[1]) >= int(sections[2]):
+            pairs_two += 1
+
+    print(pairs, pairs_two)
+
+
+
