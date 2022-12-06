82e0faa81d54be5f85bd9974b357f9b572d92ca8
azullstrom <zefaniasmusic@gmail.com>
2022-12-06 18:19:24 +0100

    Changed Day 4 Part 1

--- Day 4/main.py
+++ Day 4/main.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
         input_file[i] = input_file[i].replace('\n', '')
         sections = re.split(r',|-', input_file[i])
 
-        if int(sections[0]) <= int(sections[2]) <= int(sections[1]) >= int(sections[3]) >= int(sections[0]):
+        if int(sections[0]) <= int(sections[2]) <= int(sections[1]) and int(sections[1]) >= int(sections[3]) >= int(sections[0]):
             pairs += 1
-        elif int(sections[2]) <= int(sections[0]) <= int(sections[3]) >= int(sections[1]) >= int(sections[2]):
+        elif int(sections[2]) <= int(sections[0]) <= int(sections[3]) and int(sections[3]) >= int(sections[1]) >= int(sections[2]):
             pairs += 1
 # Part 2
         if int(sections[0]) <= int(sections[2]) <= int(sections[1]) or int(sections[1]) >= int(sections[3]) >= int(sections[0]):
