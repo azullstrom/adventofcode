2b49e35ac6907f1928e607cacaec6140ba3cbab2
Anders Ullström <anders.ullstrom@ninetech.com>
2024-12-01 22:19:37 +0100

    Part two done

--- 2024/Day 1/main.py
+++ 2024/Day 1/main.py
@@ -14,7 +14,16 @@ if __name__ == '__main__':
     for i, data in enumerate(both_sorted):
         part_one += abs(int(data[0]) - int(data[1]))
 
+    part_two = 0
+    for i in left_sorted:
+        sum = 0
+        for j in right_sorted:
+            if j in i:
+                sum += 1
+        part_two += int(i) * sum
+
     print(part_one)
+    print(part_two)
 
 
         
